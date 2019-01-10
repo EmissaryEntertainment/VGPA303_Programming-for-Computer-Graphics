84394d4e86dc3426d49717420e08a73c89c484a7
EmissaryEntertainment <43457423+EmissaryEntertainment@users.noreply.github.com>
2019-01-09 16:08:39 -0800

    Add project files.

---
+++ Week 1/HelloWorld.cpp
@@ -0,0 +1,51 @@
+//Step 1: Make sure c++ is working
+#define GLEW_STATIC
+
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW failed. \n";
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+	system("pause");
+	glfwTerminate();
+	return 0;
+}
